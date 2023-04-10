f4c246d8557e066cf0bf16c325fcc596458f608b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-04-10 23:00:41 +0700

    multiple try catch

---
+++ p14_exception/src/main/java/org/example/exceptions/ExceptionsDemo.java
@@ -0,0 +1,31 @@
+package org.example.exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class ExceptionsDemo {
+
+    public static void show() {
+
+        try {
+
+            var reader = new FileReader("file.txt");
+
+            var value = reader.read();
+
+            new SimpleDateFormat().parse("");
+            System.out.println("File opened...");
+        } catch (FileNotFoundException e) {
+
+//            System.out.println("file does not exist!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ p14_exception/src/main/java/org/example/exceptions/Main.java
@@ -0,0 +1,9 @@
+package org.example.exceptions;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ExceptionsDemo.show();
+    }
+}
