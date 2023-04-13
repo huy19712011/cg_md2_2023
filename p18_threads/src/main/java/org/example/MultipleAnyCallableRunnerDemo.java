392e34bc30a7affb45d3777af3a33de6bea4fe84
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-04-14 00:35:04 +0700

    Returning any from Threads using Callable

---
+++ p18_threads/src/main/java/org/example/MultipleAnyCallableRunnerDemo.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultipleAnyCallableRunnerDemo {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+//        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        List<CallableTask> tasks = List.of(
+                new CallableTask("C02"),
+                new CallableTask("C03"),
+                new CallableTask("C04")
+        );
+
+        String result = executorService.invokeAny(tasks);
+
+        System.out.println(result);
+
+        executorService.shutdown();
+    }
+}
