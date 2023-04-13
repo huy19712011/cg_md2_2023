e2c19e4821868d3dddb58bc05f79505525e10155
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-04-13 22:05:33 +0700

    task 1 extends Thread

--- p18_threads/src/main/java/org/example/ThreadBasicsDemo.java
+++ p18_threads/src/main/java/org/example/ThreadBasicsDemo.java
@@ -1,14 +1,29 @@
 package org.example;
 
+// extends Thread
+
+class Task1 extends Thread {
+
+    public void run() {
+
+        System.out.println("\ntask 1 started");
+        for (int i = 101; i <= 199; ++i) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n task1 done ");
+    }
+}
+
+// implements Runnable
+
 public class ThreadBasicsDemo {
 
     public static void main(String[] args) {
 
         // task 1
-        for (int i = 101; i <= 199; ++i) {
-            System.out.print(i + " ");
-        }
-        System.out.print("\n task1 done ");
+        Task1 task1 = new Task1();
+//        task1.run();
+        task1.start();
 
         // task 2
         for (int i = 201; i <= 299; ++i) {
