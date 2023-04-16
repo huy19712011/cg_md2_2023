5ec8065421f1e56f9ca9b712f6506ef0c16a35c0
huynq <huy19712011@gmail.com>
2023-04-16 10:07:10 +0700

    |-metacharacter

---
+++ p19_RegularExpression/src/main/java/org/example/MetacharactersDemo.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MetacharactersDemo {
+
+    public static void main(String[] args) {
+
+        // |- metacharacter
+        String text = "Cat Dog Fish";
+        String pattern = "cat|dog";
+
+        Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(text);
+
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
